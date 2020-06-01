59806867a1ff8c0d517f4afb66d101903d61c68d
Skarmux <ekbrah@gmail.com>
2020-06-01 20:17:03 +0200

    Re-adding XSettlers.cpp. My bad it got removed!

---
+++ src/X-Settlers.cpp
@@ -0,0 +1,54 @@
+#include "X-Settlers.h"
+
+#include "Core/Log.h"
+#include "Core/Window.h"
+#include "Renderer/Renderer2D.h"
+#include "Renderer/RenderCommand.h"
+
+
+
+#include <SDL.h>
+
+// There needs to be a X-Settlers class later on to manage closing events, game states, menues, etc...
+class XSettlers;
+
+int main(int argc, char** argv) {
+
+	Log::Init();
+	CORE_INFO("Initialized Log!");
+
+	auto win = Window::Create();
+	CORE_INFO("Created Window!");
+
+	// TODO: Set Window Event Callback
+
+	Renderer2D::Init();
+	CORE_INFO("Initialized Renderer!");
+
+	bool running = true;
+	SDL_Event event;
+
+	// enter GAME LOOP
+	while (running)
+	{
+		RenderCommand::Clear();
+
+		// TODO: draw rectangle
+		glm::vec2 position(0,0);
+		glm::vec2 size(1,1);
+		glm::vec4 color(1,1,1,1);
+		Renderer2D::DrawQuad(position, size, color);
+
+		win->OnUpdate();
+		// handle SDL event queue
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				running = false;
+			}
+		}
+	}
+
+	Renderer2D::Shutdown();
+
+	return 0;
+}
