b8173395e0c76ba7369e51fdea226ef3e5ab2ece
Skarmux <ekbrah@gmail.com>
2020-07-20 18:58:17 +0200

    simplyfying map loading and changing node buffer layout according to map file layout so that data can be copied from file with a single operation.

--- xsettlers/logic/map_loader.cpp
+++ xsettlers/logic/map_loader.cpp
@@ -1,13 +1,13 @@
-#include <filesystem>
-
 #include "logic/map_loader.h"
 #include "platform/filesystem.h"
 
+#include <filesystem>
+#include <engine.h>
+
+static const std::string MAPS_PATH = "assets/maps/s3";
+
 struct MapLoaderData
 {
-	// TODO: figuring out the proper const std::string definition approach
-	inline static const std::string MAPS_PATH = "assets/maps/s3";
-
 	std::vector<MapInfo> map_infos;
 };
 
@@ -15,8 +15,9 @@ static MapLoaderData s_data;
 
 void MapLoader::init()
 {
-	// fetch MapData for each map file within 'assets/maps/' folder
-	for (const auto& entry : std::filesystem::directory_iterator(s_data.MAPS_PATH))
+	s_data.map_infos = std::vector<MapInfo>(0);
+
+	for (const auto& entry : std::filesystem::directory_iterator(MAPS_PATH))
 	{
 		LOGIC_INFO("loading map file: {0}", entry.path().string());
 		index_map_file(entry.path().string());
@@ -28,64 +29,55 @@ void MapLoader::shutdown()
 	s_data.map_infos.clear();
 }
 
-std::vector<MapInfo> MapLoader::get_map_list()
-{
-	return s_data.map_infos;
-}
-
 void MapLoader::index_map_file(const std::string& path)
 {
-	std::vector<char> map_file = read_file(path);
+	std::vector<uint8_t> map_file = read_file(path);
 
-	MapInfo map_info;
+	MapInfo map_info{};
 	map_info.path = path;
 	map_info.parts = std::vector<PartHeader>((int)PartType::Count);
+	// TODO: Not necessary to instantiate PartHeader vector with maximum size
+	// as many part types aren't present with plenty of maps
 
-	PartHeader part_header;
+	uint8_t* buffer = map_file.data() + 8; // first partition starts at 8th byte
 
-	char* buffer = map_file.data() + 8; // first partition starts at 8th byte
+	PartHeader part_header{};
 
 	do {
 		// set partition header
-		memcpy(&part_header.type, buffer, sizeof(part_header.type));
-		memcpy(&part_header.length, buffer + 4, sizeof(part_header.length));
+		memcpy(&part_header.type, buffer, sizeof(uint32_t));
+		memcpy(&part_header.data_size, buffer + 4, sizeof(uint32_t));
 		buffer += 8;
-		part_header.offset = buffer - map_file.data();
+		part_header.data_offset = buffer - (uint8_t*)map_file.data();
 
 		// decrypt partition header
-		//part_header.type << 16;                   // bit shift
-		part_header.type = part_header.type & 0x0000FFFF; // masking
-		part_header.length = part_header.length + 0xFFFFFFF8; // masking
+		part_header.type   &= 0x0000FFFF;
+		part_header.data_size += 0xFFFFFFF8;
 
 		LOGIC_INFO("\tpartition : type {0}, length {1}, offset {2}",
-			part_header.type, part_header.length, part_header.offset
-		);
+			part_header.type, part_header.data_size, part_header.data_offset );
 		
-		if (part_header.type > 0 && part_header.type <= (int)PartType::Count)
-		{
-			//map_info.parts.push_back(part_header);
+		if (part_header.type > 0 && part_header.type <= static_cast<uint32_t>(PartType::Count) )
 			map_info.parts[part_header.type] = part_header;
-		}
 
-		// we need following information to be pre-loaded for the maps overview
-		// [MapInfo] >> player_count, resource_preset, map_mode
-		// [Area] >> size
 		switch (part_header.type)
 		{
-		case (int)PartType::MapInfo:
+		case static_cast<uint32_t>(PartType::MapInfo):
+
 			decrypt_partition(map_file, part_header);
-			memcpy(&map_info.is_campaign, buffer, sizeof(int));
-			memcpy(&map_info.player_count, buffer + 4, sizeof(int));
-			memcpy(&map_info.resource_preset, buffer + 8, sizeof(int));
+			memcpy(&map_info.is_campaign, buffer, sizeof(uint32_t));
+			memcpy(&map_info.player_count, buffer + 4, sizeof(uint32_t));
+			memcpy(&map_info.resource_preset, buffer + 8, sizeof(uint32_t));
 			break;
-		case (int)PartType::Area:
+
+		case static_cast<uint32_t>(PartType::Area):
+
 			decrypt_partition(map_file, part_header);
-			memcpy(&map_info.size, buffer, sizeof(int));
+			memcpy(&map_info.size, buffer, sizeof(uint32_t));
 			break;
 		}
 
-		//part_header.offset += (8 + part_header.length);
-		buffer += part_header.length;
+		buffer += part_header.data_size;
 
 	} while (part_header.type != 0);
 
@@ -94,50 +86,46 @@ void MapLoader::index_map_file(const std::string& path)
 	s_data.map_infos.push_back(map_info);
 }
 
-void MapLoader::decrypt_partition(std::vector<char>& map_file, const PartHeader& part_header)
+std::vector<MapInfo> MapLoader::get_map_list()
 {
-	unsigned char key = (uint32_t)part_header.type & 0xFF;
-	char* buffer = map_file.data();
-	buffer += part_header.offset;
+	// TODO: Singleton shouldn't have any getter
+	return s_data.map_infos;
+}
 
-	for (int i = 0; i < part_header.length; i++) {
-		unsigned char byt = *buffer ^ key;
+void MapLoader::decrypt_partition(std::vector<uint8_t>& map_file, const PartHeader& part_header)
+{
+	uint8_t* buffer = map_file.data();
+	buffer += part_header.data_offset;
+
+	char key = part_header.type & 0xFF;
+
+	for (uint32_t i = 0; i < part_header.data_size; i++) {
+		/*char byt = *buffer ^ key;
 		*buffer = byt;
 		buffer++;
-		key = ((key << 1) & 0xFF) ^ byt;
+		key = ((key << 1) & 0xFF) ^ byt;*/
+		*buffer ^= key;	
+		key = ((key << 1) & 0xFF) ^ *buffer;
+		buffer++;
 	}
 }
 
-void MapLoader::load_map_area(const uint32_t map_index, MapArea& map)
+void MapLoader::load_map_area(uint32_t map_index, MapNode* area)
 {	
 	const MapInfo& map_info = s_data.map_infos[map_index];
-	const PartHeader& area_part_header = map_info.parts[(int)PartType::Area];
-	const uint32_t node_count = map_info.size * map_info.size;
+	const PartHeader& area_part_header = map_info.parts[static_cast<uint32_t>(PartType::Area)];
 
-	map.accessibles.resize(node_count);
-	map.heights.resize(node_count);
-	map.objects.resize(node_count);
-	map.player_claims.resize(node_count);
-	map.resources.resize(node_count);
-	map.types.resize(node_count);
+	//area = new MapNode[(uint64_t)map_info.size * (uint64_t)map_info.size];
 
-	std::vector<char> map_file = read_file(map_info.path);
+	std::vector<uint8_t> map_file = read_file(map_info.path);
 
 	decrypt_partition(map_file, area_part_header);
 
-	uint8_t* buffer = (uint8_t*)map_file.data();
-	buffer += area_part_header.offset;
-	buffer += 4; // skip size value (4 Byte)
-	
-	for (uint32_t i = 0; i < node_count; i++)
-	{
-		map.heights[i]       = *buffer++;
-		map.types[i]         = *buffer++;
-		map.objects[i]       = *buffer++;
-		map.player_claims[i] = *buffer++;
-		map.accessibles[i]   = *buffer++;
-		map.resources[i]     = *buffer++;
-	}
+	uint8_t* buffer = map_file.data();
+	buffer += area_part_header.data_offset;
+	buffer += 4; // skip size value (4 Byte) that is already known
+
+	memcpy(area, buffer, (uint64_t)map_info.size * (uint64_t)map_info.size * sizeof(MapNode));
 
 	map_file.clear();
 }
--- xsettlers/logic/map_loader.h
+++ xsettlers/logic/map_loader.h
@@ -10,48 +10,46 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
-#include <engine.h>
-
-#include "logic/playfield.h"
-
 enum class PartType {
 	EoF = 0, MapInfo = 1, PlayerInfo = 2, TeamInfo = 3,
 	Preview = 4, Area = 6, Settlers = 7, Buildings = 8,
 	Resources = 9, QuestText = 11, QuestTip = 12, Count = 12
 };
 
-enum class MapMode {
-	// TODO: set the correct values being used in the map files
-	Conflict, Economy, Free
-};
-
 struct PartHeader
 {
 	uint32_t type;
-	uint32_t length;
-	uint32_t offset;
+	uint32_t data_size;
+	uint32_t data_offset;
+};
+
+enum class TerrainType
+{
+	// derived from the sied3 project:
+	Soil = 1, Bricks = 2, Sand = 3, Sea = 7, Grass = 16,
+	Rock = 32, SandTrace = 35, DrySandTrace = 36, Beach = 48,
+	Desert = 64, Swamp = 80, Ice = 128, Mud = 144, DarkSoil = 217,
+	Cement = 230
+};
+
+struct MapNode
+{
+	uint8_t height;
+	uint8_t type;
+	uint8_t object;
+	uint8_t player_claim;
+	uint8_t accessible;
+	uint8_t resource;
 };
 
 struct MapInfo
 {
 	std::string path;
 	uint32_t size;
-	uint8_t player_count;
-	uint8_t resource_preset;
+	uint32_t player_count;
+	uint32_t resource_preset;
 	bool is_campaign;
-	MapMode mode;
 	std::vector<PartHeader> parts;
-	// TODO: add preview image to pre-loaded map data
-};
-
-struct MapArea
-{
-	std::vector<uint8_t> heights;
-	std::vector<uint8_t> types;
-	std::vector<uint8_t> objects;
-	std::vector<uint8_t> player_claims;
-	std::vector<uint8_t> accessibles;
-	std::vector<uint8_t> resources;
 };
 
 class MapLoader
@@ -62,8 +60,8 @@ public:
 
 	static std::vector<MapInfo> get_map_list();
 
-	static void load_map_area(const uint32_t map_index, MapArea& map);
+	static void load_map_area(uint32_t map_index, MapNode* area);
 private:
 	static void index_map_file(const std::string& path);
-	static void decrypt_partition(std::vector<char>& map_file, const PartHeader& part_header);
+	static void decrypt_partition(std::vector<uint8_t>& map_file, const PartHeader& part_header);
 };
