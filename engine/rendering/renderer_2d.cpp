1976e5c0a5a7852acc8c6a14722a5547af5cd4cf
Skarmux <ekbrah@gmail.com>
2020-07-21 12:02:29 +0200

    binding buffer draw calls to the condition that the buffer contains any data. Drawing empty buffers resulted in graphical glitches.

--- engine/rendering/renderer_2d.cpp
+++ engine/rendering/renderer_2d.cpp
@@ -151,9 +151,9 @@ void Renderer2D::begin_scene(const OrthographicCamera& camera)
 	s_data.quad_shader->set_mat4("u_ViewProjection", camera.get_view_projection_matrix());
 	s_data.rhomb_shader->set_mat4("u_ViewProjection", camera.get_view_projection_matrix());
 	
-	s_data.tri_vertex_buffer_ptr   = s_data.TRI_VERTICES;
-	s_data.quad_vertex_buffer_ptr  = s_data.QUAD_VERTICES;
-	s_data.rhomb_vertex_buffer_ptr = s_data.RHOMB_VERTICES;
+	s_data.tri_vertex_buffer_ptr   = (TriVertex*)s_data.TRI_VERTICES;
+	s_data.quad_vertex_buffer_ptr  = (QuadVertex*)s_data.QUAD_VERTICES;
+	s_data.rhomb_vertex_buffer_ptr = (RhombVertex*)s_data.RHOMB_VERTICES;
 
 	s_data.tri_vertex_count   = 0;
 	s_data.quad_index_count   = 0;
@@ -166,16 +166,13 @@ void Renderer2D::end_scene()
 {
 	// push vertex data to GPU memory
 
-	uint32_t byte_size =
-		(uint8_t*)s_data.tri_vertex_buffer_ptr - (uint8_t*)s_data.TRI_VERTICES;
+	size_t byte_size = (uint8_t*)s_data.tri_vertex_buffer_ptr - (uint8_t*)s_data.TRI_VERTICES;
 	s_data.tri_vertex_buffer->push_data(s_data.TRI_VERTICES, byte_size);
 
-	byte_size = 
-		(uint8_t*)s_data.quad_vertex_buffer_ptr - (uint8_t*)s_data.QUAD_VERTICES;
+	byte_size = (uint8_t*)s_data.quad_vertex_buffer_ptr - (uint8_t*)s_data.QUAD_VERTICES;
 	s_data.quad_vertex_buffer->push_data(s_data.QUAD_VERTICES, byte_size);
 
-	byte_size =
-		(uint8_t*)s_data.rhomb_vertex_buffer_ptr - (uint8_t*)s_data.RHOMB_VERTICES;
+	byte_size = (uint8_t*)s_data.rhomb_vertex_buffer_ptr - (uint8_t*)s_data.RHOMB_VERTICES;
 	s_data.rhomb_vertex_buffer->push_data(s_data.RHOMB_VERTICES, byte_size);
 
 	flush();
@@ -183,25 +180,29 @@ void Renderer2D::end_scene()
 
 void Renderer2D::flush()
 {
-	if (s_data.quad_index_count == 0 
-		&& s_data.rhomb_index_count == 0 
-		&& s_data.tri_vertex_count == 0)
+	for (uint32_t i = 0; i < s_data.texture_slot_index; i++)
+		s_data.texture_slots[i]->bind(i);
+
+	if (s_data.quad_index_count > 0)
+	{
+		s_data.rhomb_shader->bind();
+		RenderCommand::draw_indexed(s_data.rhomb_vertex_array, s_data.rhomb_index_count);
+	}
+	else if (s_data.rhomb_index_count > 0)
+	{
+		s_data.quad_shader->bind();
+		RenderCommand::draw_indexed(s_data.quad_vertex_array, s_data.quad_index_count);
+	}
+	else if (s_data.tri_vertex_count > 0)
+	{
+		s_data.tri_shader->bind();
+		RenderCommand::draw(s_data.tri_vertex_array, s_data.tri_vertex_count);
+	}
+	else
 	{
 		RENDERER_WARN("Nothing to draw.");
 		return;
 	}
-
-	for (uint32_t i = 0; i < s_data.texture_slot_index; i++)
-		s_data.texture_slots[i]->bind(i);
-
-	s_data.tri_shader->bind();
-	RenderCommand::draw(s_data.tri_vertex_array, s_data.tri_vertex_count);
-
-	s_data.quad_shader->bind();
-	RenderCommand::draw_indexed(s_data.quad_vertex_array, s_data.quad_index_count);
-
-	s_data.rhomb_shader->bind();
-	RenderCommand::draw_indexed(s_data.rhomb_vertex_array, s_data.rhomb_index_count);
 }
 
 void Renderer2D::flush_and_reset()
@@ -219,105 +220,6 @@ void Renderer2D::flush_and_reset()
 
 	s_data.texture_slot_index = 1;
 }
-
-//void Renderer2D::draw_rhomboid_mesh(
-//	uint32_t width, float x_step, float y_step, float per_row_offset,
-//	const std::vector<std::shared_ptr<Texture2D>>& textures)
-//{
-//	if (width > s_data.MAX_MESH_WIDTH)
-//		RENDERER_ERROR(
-//			"Cannot draw mesh. Width exceeds maximum allowed. {0} > {1}",
-//			width, s_data.MAX_MESH_WIDTH);
-//
-//	// push new textures
-//	for (auto texture : textures)
-//	{
-//		float tex_index = 0.0f;
-//
-//		for (uint32_t i = 1; i < s_data.texture_slot_index; i++)
-//		{
-//			if (s_data.texture_slots[i] == texture)
-//			{
-//				// texture already slotted
-//				tex_index = (float)i;
-//				break;
-//			}
-//		}
-//
-//		if (tex_index == 0.0f)
-//		{
-//			// add texture to slots
-//			if (s_data.texture_slot_index >= Renderer2DData::MAX_TEXTURE_SLOTS)
-//				flush_and_reset();
-//
-//			tex_index = (float)s_data.texture_slot_index;
-//			s_data.texture_slots[s_data.texture_slot_index] = texture;
-//			s_data.texture_slot_index++;
-//		}
-//	}
-//
-//	uint32_t padding = (s_data.MAX_MESH_WIDTH - width) / 2;
-//	s_data.rhomb_vertex_buffer_ptr = s_data.RHOMB_VERTICES + padding + (s_data.MAX_MESH_WIDTH * padding);
-//
-//	for (uint32_t row = padding; row < s_data.MAX_MESH_WIDTH - padding; row++)
-//	{
-//		for (uint32_t col = padding; col < s_data.MAX_MESH_WIDTH - padding; col++)
-//		{
-//			s_data.rhomb_vertex_buffer_ptr->pos.x = 
-//				(float)(col * x_step) + ((s_data.MAX_MESH_WIDTH - row)*(per_row_offset));
-//			s_data.rhomb_vertex_buffer_ptr->pos.y = 
-//				(float)(s_data.MAX_MESH_WIDTH - row) * y_step;
-//			s_data.rhomb_vertex_buffer_ptr->tex = { 
-//				s_data.rhomb_vertex_buffer_ptr->pos.x / 128.0f,
-//				s_data.rhomb_vertex_buffer_ptr->pos.y / 128.0f };
-//			s_data.rhomb_vertex_buffer_ptr->pos.y += 
-//				(float)area[(width * (row - padding)) + col - padding].height;
-//
-//			/* Soil = 1, Bricks = 2, Sand = 3, Sea = 7, Grass = 16,
-//			Rock = 32, SandTrace = 35, DrySandTrace = 36, Beach = 48,
-//			Desert = 64, Swamp = 80, Ice = 128, Mud = 144, DarkSoil = 217,
-//			Cement = 230 */
-//
-//			switch (area[(width * (row - padding)) + col - padding].type)
-//			{
-//			case 0:
-//			case 1: s_data.rhomb_vertex_buffer_ptr->tex_index = 1; break; // grass
-//			case 2: 
-//			case 3: s_data.rhomb_vertex_buffer_ptr->tex_index = 3; break; // sand
-//			case 7: s_data.rhomb_vertex_buffer_ptr->tex_index = 4; break; // water
-//			case 10:
-//			case 16: s_data.rhomb_vertex_buffer_ptr->tex_index = 1; break; // grass
-//			case 17: s_data.rhomb_vertex_buffer_ptr->tex_index = 4; break; // water
-//			case 21:
-//			case 32: s_data.rhomb_vertex_buffer_ptr->tex_index = 2; break; // rock
-//			case 35:
-//			case 36:
-//			case 48:
-//			case 64: s_data.rhomb_vertex_buffer_ptr->tex_index = 3; break; // sand
-//			default: s_data.rhomb_vertex_buffer_ptr->tex_index = 1;
-//			}
-//			
-//			
-//			if (row < s_data.MAX_MESH_WIDTH - padding - 1)
-//			{
-//				uint8_t top = area[(width * (row - padding)) + col - padding].height;
-//				uint8_t left = area[(width * (row - padding + 1)) + col - padding].height;
-//				s_data.rhomb_vertex_buffer_ptr->col =
-//					0.875f + (float)(top - left) * 0.1f;
-//			}
-//			else
-//				s_data.rhomb_vertex_buffer_ptr->col = 1.0f;
-//
-//			s_data.rhomb_vertex_buffer_ptr->pos.x -= 
-//				(float)s_data.MAX_MESH_WIDTH * x_step * 1.5f / 2.0;
-//			s_data.rhomb_vertex_buffer_ptr->pos.y -= 
-//				(float)s_data.MAX_MESH_WIDTH * y_step * 0.5f;
-//
-//			s_data.rhomb_vertex_buffer_ptr++;
-//		}
-//		s_data.rhomb_vertex_buffer_ptr += (2*padding);
-//	}
-//}
 
 void Renderer2D::assign_texture_slot(const std::shared_ptr<Texture2D>& texture, uint32_t& tex_index)
 {
@@ -441,7 +343,7 @@ void Renderer2D::draw_rhomboid(
 	s_data.rhomb_index_count += 6;
 }
 
-void Renderer2D::submit(TriVertex* buffer, uint32_t count, const std::shared_ptr<Texture2D>& texture)
+void Renderer2D::submit(TriVertex* const buffer, uint32_t count, const std::shared_ptr<Texture2D>& texture)
 {
 	if (s_data.tri_vertex_count >= Renderer2DData::MAX_TRI_VERTICES)
 	{
@@ -462,7 +364,7 @@ void Renderer2D::submit(TriVertex* buffer, uint32_t count, const std::shared_ptr
 	}
 	else
 	{*/
-	memcpy(s_data.tri_vertex_buffer_ptr, buffer, count * sizeof(TriVertex));
+	memcpy(s_data.tri_vertex_buffer_ptr, buffer, (size_t)count * sizeof(TriVertex));
 	//}
 
 	s_data.tri_vertex_buffer_ptr += count;
