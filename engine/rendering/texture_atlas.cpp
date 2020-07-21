edbfbea90b23ef9415bdd67c808f0d793134dc5d
Skarmux <ekbrah@gmail.com>
2020-07-21 23:43:18 +0200

    added functionality to transform local tex coords to atlas tex coords

--- engine/rendering/texture_atlas.cpp
+++ engine/rendering/texture_atlas.cpp
@@ -3,10 +3,12 @@
 //#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
+#include <math.h>
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
 
-void TextureAtlas::add_texture(const std::string& path, uint32_t& texture_id)
+void TextureAtlas::add_texture(const std::string& path)
 {
 	int width, height, channels;
 	stbi_uc* data = stbi_load(path.c_str(), &width, &height, &channels, 0);
@@ -15,15 +17,14 @@ void TextureAtlas::add_texture(const std::string& path, uint32_t& texture_id)
 		path, width, height, channels);
 	// 32-bit color -> Pixel [R:4B, G:4B, B:4B, A:4B] -> [16 Byte]
 	// 32-bit color -> Pixel [R:4B, G:4B, B:4B]       -> [12 Byte]
-	add_texture((uint32_t*)data, width, height, texture_id);
+	add_texture((uint32_t*)data, width, height);
 }
 
 void TextureAtlas::add_texture(
-	uint32_t* data, uint32_t width, uint32_t height, uint32_t& texture_id)
+	uint32_t* data, uint32_t width, uint32_t height)
 {	
 	m_texture_spaces.emplace_back(Space{ 0, 0, width, height });
 	m_texture_data.push_back(data);
-	texture_id = m_texture_spaces.size() - 1;
 }
 
 bool TextureAtlas::insert_texture(
@@ -108,14 +109,14 @@ bool TextureAtlas::insert_texture(
 	return false;
 }
 
-std::shared_ptr<Texture2D> TextureAtlas::create_atlas_texture(
+std::shared_ptr<Texture2D> TextureAtlas::create_texture(
 	uint32_t max_width, uint32_t max_height)
 {
-	uint32_t atlas_width = max_width;
-	uint32_t atlas_height = max_height;
+	m_width = max_width;
+	m_height = max_height;
 
 	std::vector<Space> free_spaces;
-	free_spaces.emplace_back(Space{ 0, 0, atlas_width, atlas_height });
+	free_spaces.emplace_back(Space{ 0, 0, m_width, m_height });
 
 	std::vector<Space*> texture_space_ptrs(m_texture_spaces.size());
 	for (uint32_t i = 0; i < m_texture_spaces.size(); i++)
@@ -162,7 +163,7 @@ std::shared_ptr<Texture2D> TextureAtlas::create_atlas_texture(
 	}
 	
 	uint32_t atlas_channels = 4; // RGBA 4*32b
-	uint32_t* atlas_data = new uint32_t[atlas_width * atlas_height * atlas_channels];
+	uint32_t* atlas_data = new uint32_t[m_width * m_height * atlas_channels];
 
 	for (uint32_t i = 0; i < m_texture_spaces.size(); i++)
 	{
@@ -172,7 +173,7 @@ std::shared_ptr<Texture2D> TextureAtlas::create_atlas_texture(
 		for (uint32_t row = 0; row < texture_space.height; row++)
 		{
 			uint32_t atlas_data_offset = 
-				(atlas_width * (texture_space.y + row)) + texture_space.x;
+				(m_width * (texture_space.y + row)) + texture_space.x;
 			uint32_t texture_data_offset = texture_space.width * row;
 			memcpy(
 				atlas_data + atlas_data_offset, 
@@ -183,14 +184,36 @@ std::shared_ptr<Texture2D> TextureAtlas::create_atlas_texture(
 	}
 	m_texture_data.clear();
 
-	std::shared_ptr<Texture2D> texture = Texture2D::create(atlas_width, atlas_height);
+	std::shared_ptr<Texture2D> texture = Texture2D::create(m_width, m_height);
 	texture->push_data(
-		atlas_data, atlas_width * atlas_height * atlas_channels * sizeof(uint32_t));
+		atlas_data, m_width * m_height * atlas_channels * sizeof(uint32_t));
 
 	stbi_write_png("C:/Users/Nils/Desktop/atlas.png",
-		atlas_width, atlas_height, 4, atlas_data, atlas_width * 4);
+		m_width, m_height, 4, atlas_data, m_width * 4);
 
 	delete[] atlas_data;
 
 	return texture;
+}
+
+void TextureAtlas::translate_to_atlas_coords(uint32_t tex_id, glm::vec2& tex_coord) const
+{
+	if (m_width == 0 || m_height == 0)
+	{
+		RENDERER_ERROR("TextureAtlas composition has yet to be created. Call create_texture() before this.");
+		return;
+	}
+
+	const Space& tex_space = m_texture_spaces[tex_id];
+
+	// draw coordinates a tiny bit on the inside to avoid texture bleeding
+	if (tex_coord.x == 0.0f) tex_coord.x += 0.01;
+	if (tex_coord.x == 1.0f) tex_coord.x -= 0.01;
+	if (tex_coord.y == 0.0f) tex_coord.y += 0.01;
+	if (tex_coord.y == 1.0f) tex_coord.y -= 0.01;
+
+	// bring local tex_coords into local pixel space, add atlas pixel offset
+	// and divide by atlas width/height to get the final uv coord within atlas
+	tex_coord.x = ( (tex_coord.x * tex_space.width ) + tex_space.x) / m_width;
+	tex_coord.y = ( (tex_coord.y * tex_space.height) + tex_space.y) / m_height;
 }
--- engine/rendering/texture_atlas.h
+++ engine/rendering/texture_atlas.h
@@ -8,21 +8,22 @@
 class TextureAtlas
 {
 public:
+	TextureAtlas() : m_width(0), m_height(0) {}
+	void translate_to_atlas_coords(uint32_t tex_id, glm::vec2& tex_coord) const;
+
+	void add_texture(const std::string& path);
+	void add_texture(uint32_t* data, uint32_t width, uint32_t height);
+
+	std::shared_ptr<Texture2D> create_texture(uint32_t width, uint32_t height);
+private:
 	struct Space
 	{
 		uint32_t x, y;
 		uint32_t width, height;
 	};
-public:
-	TextureAtlas::Space get_texture_space(const uint32_t texture_id) const { return m_texture_spaces[texture_id]; };
-
-	void add_texture(const std::string& path, uint32_t& texture_id);
-	void add_texture(uint32_t* data, uint32_t width, uint32_t height, uint32_t& texture_id);
-
-	std::shared_ptr<Texture2D> create_atlas_texture(uint32_t width, uint32_t height);
-private:
 	bool insert_texture(std::vector<Space>& free_spaces, Space* texture);
 private:
+	uint32_t m_width, m_height;
 	std::vector<Space> m_texture_spaces;
 	std::vector<uint32_t*> m_texture_data;
 };
